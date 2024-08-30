89a1e9984c57f2e91ac51cab7116d35b083249da
Ja <makej1318@gmail.com>
2024-08-31 01:49:00 +0500

    last

--- 1start/Objects/tipizatsiya.js
+++
@@ -1,7 +0,0 @@
-// олд
-console.log(typeof(String(null)));
-console.log(typeof(String(4)));
-
-
-// конкатинация
-console.log(typeof(4 + ''));
--- 1start/интерполяция.js
+++
@@ -1,4 +0,0 @@
-const real = "Ivan";
-
-console.log(`htpps:/...${real}/yes`);
-alert(`hi,${real}`)
